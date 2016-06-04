920b731a3ead31d50553f17a9fb44ad9bfb01615
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-06-04 12:08:31 +0200

    plot word clouds (example PNGs from 2016-Jun-04)

--- FreifunkVis-wordclouds.R
+++ FreifunkVis-wordclouds.R
@@ -17,11 +17,27 @@ FF_prepareWordcloud = function(x) {
 FF_wordPal = c(brewer.pal(n = 8, name = "Dark2"),
                brewer.pal(n = 8, name = "Set1"))
 
+FF_plotWordcloud = function(x, filename = "FF_wordcloud.png") {
+  png(file = filename, width = 960, height = 960, res = 300)
+  print(wordcloud(colnames(x), 
+                  colSums(x),
+                  colors = FF_wordPal,
+                  random.color = T,
+                  max.words = length(FF_wordPal), 
+                  min.freq = 2)
+        )
+  dev.off()
+}
+
 # service name cloud; learned from http://stackoverflow.com/a/29852089
 FFServices = FFDF[, grepl("services.serviceName", names(FFDF))]
 FFServicesTM = FF_prepareWordcloud(x = FFServices)
+FF_plotWordcloud(x = FFServicesTM, filename = "FF_service_cloud.png")
 
 # description cloud 
 FFDescriptionsTM = FF_prepareWordcloud(x = FFDF$state.description)
 FFDescriptionsTM = FFDescriptionsTM[, setdiff(names(FFDescriptionsTM), 
                                           c("freifunk", "freifnunk", "freifunker_innen", "freifunkerinnen"))]
+FF_plotWordcloud(x = FFDescriptionsTM, filename = "FF_description_cloud.png")
+
+# [ ] uplod PNGs to webserver for static linking
