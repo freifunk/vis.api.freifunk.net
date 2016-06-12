077c39443c844f6583f4f1b966e7c16f2d3191f3
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-06-12 11:18:08 +0200

    rename data frames, term matrices, konstants & functions…
    …acording to https://google.github.io/styleguide/Rguide.xml

--- FreifunkVis-wordclouds.R
+++ FreifunkVis-wordclouds.R
@@ -5,7 +5,7 @@ library(RColorBrewer)
 library(tm)
 library(wordcloud)
 
-FF_prepareWordcloud <- function(x, remPunct = TRUE) {
+ff.prepareWordcloud <- function(x, remPunct = TRUE) {
   df <- Corpus(VectorSource(x))
   if (remPunct == TRUE) { df <- tm_map(df, removePunctuation) }
   df <- tm_map(df, removeWords, stopwords("de"))
@@ -13,31 +13,31 @@ FF_prepareWordcloud <- function(x, remPunct = TRUE) {
 }
 
 # all RColerBrewer qualitative palettes
-FF_wordPal <- c(brewer.pal(n = 8, name = "Dark2"),
-                brewer.pal(n = 8, name = "Set1"))
+ffk.palette <- c(brewer.pal(n = 8, name = "Dark2"),
+                 brewer.pal(n = 8, name = "Set1"))
 
-FF_plotWordcloud <- function(x, filename = "FF_wordcloud.png") {
+ff.plotWordcloud <- function(x, filename = "FF_wordcloud.png") {
   png(file = filename, width = 960, height = 960, res = 300)
   print(
     wordcloud(colnames(x), 
               colSums(x),
-              colors       = FF_wordPal,
+              colors       = ffk.palette,
               random.color = T,
-              max.words    = length(FF_wordPal), 
+              max.words    = length(ffk.palette), 
               min.freq     = 2
     ))
   dev.off()
 }
 
 # service name cloud; learned from http://stackoverflow.com/a/29852089
-FFServices <- FFDF[, grepl("services.serviceName", names(FFDF))]
-FFServicesTM <- FF_prepareWordcloud(x = FFServices)
-FF_plotWordcloud(x = FFServicesTM, filename = "FF_service_cloud.png")
+ffdf.services <- ffdf[, grepl("services.serviceName", names(ffdf))]
+fftm.services <- ff.prepareWordcloud(x = ffdf.services)
+ff.plotWordcloud(x = fftm.services, filename = "FF_service_cloud.png")
 
 # description cloud 
-FFDescriptionsTM <- FF_prepareWordcloud(x = FFDF$state.description)
-FFDescriptionsTM <- FFDescriptionsTM[, setdiff(names(FFDescriptionsTM), 
-                                               c("freifunk", "freifnunk", "freifunker_innen", "freifunkerinnen"))]
-FF_plotWordcloud(x = FFDescriptionsTM, filename = "FF_description_cloud.png")
+fftm.descriptions <- ff.prepareWordcloud(x = ffdf$state.description)
+fftm.descriptions <- fftm.descriptions[, setdiff(names(fftm.descriptions), 
+                                                 c("freifunk", "freifnunk", "freifunker_innen", "freifunkerinnen"))]
+ff.plotWordcloud(x = fftm.descriptions, filename = "FF_description_cloud.png")
 
 # [ ] uplod PNGs to webserver for static linking
--- FreifunkVis.R
+++ FreifunkVis.R
@@ -8,69 +8,69 @@ library(plyr) # rbind.fill
 library(RCurl) # getURL
 
 # read in & clean JSON file (FF = Freifunk, 3rd F = file)
-FFF <- getURL("https://api.freifunk.net/data/ffSummarizedDir.json")
-FFF <- gsub(
+ffk.currentJSON <- getURL("https://api.freifunk.net/data/ffSummarizedDir.json")
+ffk.currentJSON <- gsub(
   pattern     = "\r\n",  # line-breaks
   replacement = " ", 
-  x           = FFF
+  x           = ffk.currentJSON
 )
 
-# basic data frame (DF) cleaning
-FF_cleanDF <- function(DF) {
+# basic data frame cleaning
+ff.cleanDF <- function(x) {
   
   # convert some variables to more useful formats
-  DF$mtime <- as.Date(DF$mtime, format = "%F %H:%M:%OS") # %F = %Y-%m-%d (ISO8601)
-  DF$state.nodes <- as.numeric(DF$state.nodes)
-  DF$timeline.timestamp <- as.Date(DF$timeline.timestamp, format = "%F")
+  x$mtime <- as.Date(x$mtime, format = "%F %H:%M:%OS") # %F = %Y-%m-%d (ISO8601)
+  x$state.nodes <- as.numeric(x$state.nodes)
+  x$timeline.timestamp <- as.Date(x$timeline.timestamp, format = "%F")
   
   # very noisy time formats, using most common ("%F T%H:%M:%OS" => few more NA's)
-  DF$state.lastchange <- as.Date(DF$state.lastchange, format = "%F")
+  x$state.lastchange <- as.Date(x$state.lastchange, format = "%F")
   
-  return(DF)
+  return(x)
 }
 
 # convert JSON to data frame; learned from https://stackoverflow.com/a/27432542
-FF_readJSONs = function(JSON) {
+ff.readJSONs = function(JSONs) {
   return(
     rbind.fill( 
-        JSON, 
       lapply(
         lapply(
+          JSONs, 
           function(x) unlist(x)), 
         function(x) do.call("data.frame", as.list(x))
       )))
 }
 
 # generate basic data frame for testing
-FFF <- fromJSON(FFF)
-FFDF <- FF_readJSONs(FFF)
-FFDF <- FF_cleanDF(FFDF)
+ffk.currentJSON <- fromJSON(ffk.currentJSON)
+ffdf <- ff.readJSONs(ffk.currentJSON)
+ffdf <- ff.cleanDF(ffdf)
 
 # further data processing & visualisations in separate files.
 source(file = "FreifunkVis-timeseries.R")
 source(file = "FreifunkVis-wordclouds.R")
 
 # routing protocols
-FF_current_protocols <- as.data.frame(
+ffdf.currentProtcols <- as.data.frame(
   colSums(
-    FF_prepareWordcloud(
-      FFDF[ , grepl("techDetails.routing", 
-                   names(FFDF))], 
+    ff.prepareWordcloud(
+      ffdf[ , grepl("techDetails.routing", 
+                    names(ffdf))], 
       remPunct = FALSE)
-    ))
+  ))
 
 # expand to proper data frame, instead of named rows
-names(FF_current_protocols) <- "used"
-FF_current_protocols$protocol <- as.factor(rownames(FF_current_protocols))
+names(ffdf.currentProtcols) <- "used"
+ffdf.currentProtcols$protocol <- as.factor(rownames(ffdf.currentProtcols))
 
 # learned from https://trinkerrstuff.wordpress.com/2013/08/14/how-do-i-re-arrange-ordering-a-plot-revisited/
-FF_current_protocols$protocol <- factor(
-  x      = FF_current_protocols$protocol, 
-  levels = FF_current_protocols$protocol[order(FF_current_protocols$used)])
-FF_current_protocols$fraction <- FF_current_protocols$used/sum(FF_current_protocols$used)
+ffdf.currentProtcols$protocol <- factor(
+  x      = ffdf.currentProtcols$protocol, 
+  levels = ffdf.currentProtcols$protocol[order(ffdf.currentProtcols$used)])
+ffdf.currentProtcols$fraction <- ffdf.currentProtcols$used/sum(ffdf.currentProtcols$used)
 
-FFP_current_protocols <- ggplot(
-  data = FF_current_protocols, 
+ffp.currentProtcols <- ggplot(
+  data = ffdf.currentProtcols, 
   mapping = aes(x     = protocol, 
                 y     = fraction, 
                 color = protocol)
@@ -78,26 +78,26 @@ FFP_current_protocols <- ggplot(
   geom_point() + 
   scale_y_continuous(labels = percent_format(), 
                      limits = c(0, # ensure dynamic resizin of y-axis, with...
-                                max(FF_current_protocols$fraction)*1.1) # ... upper tick mark
-  scale_color_manual(values = FF_wordPal) +
+                                max(ffdf.currentProtcols$fraction)*1.1) # ... upper tick mark
   ) +
+  scale_color_manual(values = ffk.palette) +
   labs(title = "Fractions of currently used routing protocols", 
        x = NULL, y = NULL, color = NULL) + 
   theme_classic()
-FFP_current_protocols
+ffp.currentProtcols
 
 ggsave(filename = "FF_protocols.png", 
-       plot = FFP_current_protocols)
+       plot = ffp.currentProtcols)
 
 # sort protocols by popularity & remove rare for cleaner pie chart
-FF_current_protocols <- FF_current_protocols[order(FF_current_protocols$used),] 
-FF_current_common_protocols <- subset(FF_current_protocols, used > 1)
+ffdf.currentProtcols <- ffdf.currentProtcols[order(ffdf.currentProtcols$used),] 
+ffdf.currentCommonProtocols <- subset(ffdf.currentProtcols, used > 1)
 
 # plot acceptable pie chart; learned from http://www.randalolson.com/2016/03/24/the-correct-way-to-use-pie-charts/
 png(file = "FF_protocols_pie.png", width = 960, height = 960, res = 300)
-print(pie(x         = FF_current_common_protocols$used, 
-          labels    = rownames(FF_current_common_protocols),
-          col       = FF_wordPal,
+print(pie(x         = ffdf.currentCommonProtocols$used, 
+          labels    = rownames(ffdf.currentCommonProtocols),
+          col       = ffk.palette,
           clockwise = T
 ))
 dev.off()
