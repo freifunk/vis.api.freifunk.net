c6b21fd2a85b792d076aac52946388247266fe62
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2016-06-19 09:21:26 +0200

    rename ambiguous parameters

--- FreifunkVis-wordclouds.R
+++ FreifunkVis-wordclouds.R
@@ -12,13 +12,13 @@ ffk.palette <- c(brewer.pal(n = 8, name = "Dark2"),
 
 # service name cloud; learned from http://stackoverflow.com/a/29852089
 ffdf.services <- ffdf[, grepl("services.serviceName", names(ffdf))]
-fftm.services <- ff.prepareWordcloud(x = ffdf.services)
-ff.plotWordcloud(x = fftm.services, filename = "FF_service_cloud.png")
+fftm.services <- ff.prepareWordcloud(text = ffdf.services)
+ff.plotWordcloud(terms = fftm.services, filename = "FF_service_cloud.png")
 
 # description cloud 
-fftm.descriptions <- ff.prepareWordcloud(x = ffdf$state.description)
+fftm.descriptions <- ff.prepareWordcloud(text = ffdf$state.description)
 fftm.descriptions <- fftm.descriptions[, setdiff(names(fftm.descriptions), 
                                                  c("freifunk", "freifnunk", "freifunker_innen", "freifunkerinnen"))]
-ff.plotWordcloud(x = fftm.descriptions, filename = "FF_description_cloud.png")
+ff.plotWordcloud(terms = fftm.descriptions, filename = "FF_description_cloud.png")
 
 # [ ] uplod PNGs to webserver for static linking
--- ffFunctions.R
+++ ffFunctions.R
@@ -5,35 +5,35 @@ ff.readJSONs = function(JSONs) {
       lapply(
         lapply(
           JSONs, 
-          function(x) unlist(x)), 
-        function(x) do.call("data.frame", as.list(x))
+          function(JSON) unlist(JSON)), 
+        function(JSON) do.call("data.frame", as.list(JSON))
       )))
 }
 
 # basic data frame cleaning
-ff.cleanDF <- function(x) {
+ff.cleanDF <- function(df) {
   
   # convert some variables to more useful formats
-  x$mtime <- as.Date(x$mtime, format = "%F %H:%M:%OS") # %F = %Y-%m-%d (ISO8601)
-  x$state.nodes <- as.numeric(x$state.nodes)
-  x$timeline.timestamp <- as.Date(x$timeline.timestamp, format = "%F")
+  df$mtime <- as.Date(df$mtime, format = "%F %H:%M:%OS") # %F = %Y-%m-%d (ISO8601)
+  df$state.nodes <- as.numeric(df$state.nodes)
+  df$timeline.timestamp <- as.Date(df$timeline.timestamp, format = "%F")
   
   # very noisy time formats, using most common ("%F T%H:%M:%OS" => few more NA's)
-  x$state.lastchange <- as.Date(x$state.lastchange, format = "%F")
+  df$state.lastchange <- as.Date(df$state.lastchange, format = "%F")
   
-  return(x)
+  return(df)
 }
 
 # convert text in data frame to a sparse term matrix
-ff.prepareWordcloud <- function(x, remPunct = TRUE) {
-  df <- Corpus(VectorSource(x))
+ff.prepareWordcloud <- function(text, remPunct = TRUE) {
+  df <- Corpus(VectorSource(text))
   if (remPunct == TRUE) { df <- tm_map(df, removePunctuation) }
   df <- tm_map(df, removeWords, stopwords("de"))
   return(as.data.frame(as.matrix(DocumentTermMatrix(df))))
 }
 
-ff.plotWordcloud <- function(x, filename = "FF_wordcloud.png") {
-  png(file = filename, width = 960, height = 960, res = 300)
+ff.plotWordcloud <- function(terms, filename = "FF_wordcloud.png") {
+  png(filename, width = 960, height = 960, res = 300)
   print(
     wordcloud(colnames(x), 
               colSums(x),
