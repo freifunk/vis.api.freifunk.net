d4fc4406ae951aee1ab4729c8efb58fc02337b01
Pierre Marshall <pierre.marshall@gmail.com>
2024-06-07 11:01:57 +0100

    connect to the database, insert a record
    following https://www.mongodb.com/docs/drivers/rust/current/usage-examples/findOne/

--- json_adder/src/main.rs
+++ json_adder/src/main.rs
@@ -5,7 +5,7 @@ use mongodb::{
 };
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
-    let uri = "mongodb://ADMIN:PASSWORD@localhost:27017";
+    let uri: &str = "mongodb://ADMIN:PASSWORD@localhost:27017";
 
     let mut client_options = ClientOptions::parse_async(uri).await?;
 
@@ -14,15 +14,25 @@ async fn main() -> mongodb::error::Result<()> {
     client_options.server_api = Some(server_api);
 
     // Create a new client and connect to the server
-    let client = Client::with_options(client_options)?;
+    let client: Client = Client::with_options(client_options)?;
+
+    let database = client.database("communities");
+    let collection = database.collection("hourlySnapshot");
 
     // Send a ping to confirm a successful connection
     client
-        .database("commufdfdfddnities")
+        .database("communities")
         .run_command(doc! { "ping": 1 }, None)
         .await?;
 
     println!("Pinged your deployment. You successfully connected to MongoDB!");
+
+    let doc = doc! {
+        "title": "Mistress America", "type": "movsssie"
+    };
+    let result = collection.insert_one(doc, None).await?;
+
+    println!("{:#?}", result);
 
     Ok(())
 }
