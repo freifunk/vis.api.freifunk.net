a20174657667494178444ebb3b6177bcc06cc549
Pierre Marshall <pierre.marshall@gmail.com>
2024-06-10 11:50:19 +0100

    feat: read file to string in json_adder example
    early steps, if the file path is wrong it'll give a system error

--- json_adder/examples/json_to_struct.rs
+++ json_adder/examples/json_to_struct.rs
@@ -1,3 +1,17 @@
 // #[derive(Serialize, Deserialize)]
-// let schema_file = fs::read_to_string("0.5.2.json")?;
-// let address_schema = Schema::try_from(schema_file)?;
+// use std::env;
+use std::fs;
+use std::error::Error;
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file_path: &str =
+        "../../api.freifunk.net/data/history/20240129-10.01.02-ffSummarizedDir.json";
+
+    // println!("In file {}", file_path);
+
+    let contents: String = fs::read_to_string(file_path)?;
+
+    // println!("With text:\n{contents}");
+    Ok(())
+}
