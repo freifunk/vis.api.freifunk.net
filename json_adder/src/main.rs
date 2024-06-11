aabed8720fcf936d0a99975cb798a1622094b77e
Pierre Marshall <pierre.marshall@gmail.com>
2024-06-11 22:06:57 +0100

    style: comment and format this code

--- json_adder/src/main.rs
+++ json_adder/src/main.rs
@@ -1,25 +1,17 @@
+use bson::Document;
 use mongodb::{
     bson,
     options::{ClientOptions, ServerApi, ServerApiVersion},
     Client,
 };
-use serde::{Serialize, Deserialize};
+// use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::fs;
-use bson::Document;
-
-// Next steps here:
-// Scan through json files in directory < -- do this last
-// for each file: read it into a struct
-// insert record into the database
-
-// ../../data/history/20240129-10.01.02-ffSummarizedDir.json
 
 #[tokio::main]
 
 async fn main() -> mongodb::error::Result<()> {
     let uri: &str = "mongodb://ADMIN:PASSWORD@localhost:27017";
-
     let mut client_options = ClientOptions::parse_async(uri).await?;
 
     // Set the server_api field of the client_options object to Stable API version 1
@@ -29,18 +21,26 @@ async fn main() -> mongodb::error::Result<()> {
     // Create a new client and connect to the server
     let client: Client = Client::with_options(client_options)?;
 
+    // Connect to the database and the snapshot collection document
     let database = client.database("communities");
     let snapshot_collection = database.collection("hourlySnapshot");
 
     println!("Pinged your deployment. You successfully connected to MongoDB!");
 
+    // File path for the json, change this later
     let file_path: &str =
         "../../api.freifunk.net/data/history/20240129-10.01.02-ffSummarizedDir.json";
 
+    // Convert JSON to string, then to value, then to bson
     let contents: String = fs::read_to_string(file_path).expect("couldn't read file");
     let value: Value = serde_json::from_str(&contents).expect("couldn't parse json");
-    let bson_doc = bson::to_bson(&value).expect("couldn't convert value to bson").as_document().unwrap().clone();
+    let bson_doc: Document = bson::to_bson(&value)
+        .expect("couldn't convert value to bson")
+        .as_document()
+        .unwrap()
+        .clone();
 
+    // Insert this the bson into the collection
     let result = snapshot_collection.insert_one(bson_doc, None).await?;
 
     println!("Inserted a document with _id: {}", result.inserted_id);
