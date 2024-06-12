2ca93f33b2a69631ad21aa1f0c2532770550f46d
Pierre Marshall <pierre.marshall@gmail.com>
2024-06-12 16:03:05 +0100

    feat: Add communities as individual objects from the json file
    Loop through the json and add each community as its own object, and add a timestamp

--- json_adder/src/main.rs
+++ json_adder/src/main.rs
@@ -1,10 +1,14 @@
+#[allow(unused_imports)]
+#[allow(dead_code)]
 use bson::Document;
+use chrono::{NaiveDateTime, Utc};
+use core::fmt;
 use mongodb::{
     bson,
     options::{ClientOptions, ServerApi, ServerApiVersion},
     Client,
 };
-// use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::fs;
 
@@ -25,25 +29,63 @@ async fn main() -> mongodb::error::Result<()> {
     let database = client.database("communities");
     let snapshot_collection = database.collection("hourlySnapshot");
 
-    println!("Pinged your deployment. You successfully connected to MongoDB!");
-
-    // File path for the json, change this later
+    // File path for sample json file, change this later
     let file_path: &str =
         "../../api.freifunk.net/data/history/20240129-10.01.02-ffSummarizedDir.json";
 
     // Convert JSON to string, then to value, then to bson
     let contents: String = fs::read_to_string(file_path).expect("couldn't read file");
     let value: Value = serde_json::from_str(&contents).expect("couldn't parse json");
-    let bson_doc: Document = bson::to_bson(&value)
-        .expect("couldn't convert value to bson")
-        .as_document()
-        .unwrap()
-        .clone();
+
+    #[derive(Serialize, Deserialize, Debug)]
+    struct Community {
+        label: String,
+        timestamp: bson::DateTime,
+        content: Value,
+    }
+
+    // Construct bson datetime function
+    fn mtime_to_bson (mtime: &str) -> bson::DateTime {
+        // mtime is surrounded by quotes, and when passed into parse_from_str, it is
+        // cut down to the format described in fmt
+        let fmt = "%Y-%m-%d %H:%M:%S";
+        let chrono_dt: chrono::DateTime<Utc> = NaiveDateTime::parse_from_str(&mtime[1..20], fmt)
+            .expect("failed to parse naive datetime from json value")
+            .and_utc();
+        // Convert to bson datetime, copied
+        // from https://docs.rs/bson/latest/bson/struct.DateTime.html
+        let bson_dt: bson::DateTime = chrono_dt.into();
+        bson::DateTime::from_chrono(chrono_dt);
+        bson_dt
+    }
+
+    for (community_label, community_info) in value.as_object().unwrap() {
+       
+        let mtime = &community_info["mtime"].to_string();
+        let bson_time = mtime_to_bson(mtime);
+        println!("{:?}", bson_time);
+
+        let community = Community {
+            label: community_label.to_string(),
+            timestamp: bson_time,
+            content: community_info.clone()
+        };
+
+
+        // let bson_doc: Document = bson::to_bson(&value)
+    //     .expect("couldn't convert value to bson")
+    //     .as_document()
+    //     .unwrap()
+    //     .clone();
 
     // Insert this the bson into the collection
-    let result = snapshot_collection.insert_one(bson_doc, None).await?;
+    let result = snapshot_collection.insert_one(community, None).await?;
 
     println!("Inserted a document with _id: {}", result.inserted_id);
+    
+    }
+
+
 
     Ok(())
 }
