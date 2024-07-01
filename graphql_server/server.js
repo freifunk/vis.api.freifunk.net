82bc6614720555f3cd01034149e08e56183e26c1
extua <pierre.marshall@gmail.com>
2024-07-01 14:32:36 +0000

    add server.js from graphql tutorial, fixes #31
    getting started https://graphql.org/graphql-js/

---
+++ graphql_server/server.js
@@ -0,0 +1,32 @@
+var express = require("express")
+var { createHandler } = require("graphql-http/lib/use/express")
+var { buildSchema } = require("graphql")
+ 
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+ 
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello() {
+    return "Hello world!"
+  },
+}
+ 
+var app = express()
+ 
+// Create and use the GraphQL handler.
+app.all(
+  "/graphql",
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  })
+)
+ 
+// Start the server at port
+app.listen(4000)
+console.log("Running a GraphQL API server at http://localhost:4000/graphql")
