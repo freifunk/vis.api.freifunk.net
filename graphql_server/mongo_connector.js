582be27980423cf69c448a7dc0c9aae7af653481
extua <pierre.marshall@gmail.com>
2024-07-02 14:38:12 +0000

    remove unnecessary comment from mongoose test file

--- graphql_server/mongo_connector.js
+++ graphql_server/mongo_connector.js
@@ -5,5 +5,4 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://ADMIN:PASSWORD@localhost:27017');
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
