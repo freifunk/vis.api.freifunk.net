cc9fe5a410e45e42b1eadda893b71959657a19ae
Pierre Marshall <pierre.marshall@gmail.com>
2024-07-29 15:18:10 +0100

    tentative schema for grouped timeseries_nodes_per_community

--- graphql_server/schema.js
+++ graphql_server/schema.js
@@ -5,7 +5,7 @@ const SDLschema = `
 type Query {
   communities(metadata: String): [Community!]!
   latest_nodes_per_community(metadata: String): [Community!]!
-  timeseries_nodes_per_community(metadata: String): [Community!]!
+  timeseries_nodes_per_community(metadata: String): [NodesTimeseries!]!
 }
 
 type Community {
@@ -15,6 +15,22 @@ type Community {
   # This adds a nodes field for latest_communities
   # there is a better way of doing this with interfaces
   nodes: Int
+}
+
+type NodesTimeseries {
+  _id: Date
+  sumNodes: Int
+}
+
+type Date {
+  date: DateParts
+}
+
+type DateParts {
+  year: Int
+  month: Int
+  day: Int
+  hour: Int
 }
 
 `;
