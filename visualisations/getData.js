ec3794e7fdd4c2970aeffd502ac4753ce4543421
Pierre Marshall <pierre.marshall@gmail.com>
2024-07-31 18:01:24 +0100

    move data getting function into its own file
    it returns the first object in json.data, at this point these functions are more generic and can be used interchangeably for different graphs

---
+++ visualisations/getData.js
@@ -0,0 +1,36 @@
+// fetch data from graphql endpoint
+async function getRemoteData(query) {
+    const url = "http://localhost:4000/api";
+    console.log(query);
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        const myRequest = new Request(url, {
+            method: "POST",
+            body: query,
+            headers: myHeaders,
+        });
+        const response = await fetch(myRequest);
+        console.log(myRequest);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        return Object.values(json.data)[0];
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+async function getLocalData() {
+    try {
+        const response = await fetch("test_response.json");
+        const json = await response.json();
+        return json.data.latest_nodes_per_community;
+    }
+    catch (error) {
+        console.error(error.message);
+    }
+}
+
+export {getRemoteData, getLocalData};
