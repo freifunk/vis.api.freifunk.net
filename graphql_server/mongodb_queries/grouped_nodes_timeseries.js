c6d3332dc2443401edb0ec1472b99e53eb906117
Pierre Marshall <pierre.marshall@gmail.com>
2024-08-08 16:04:28 +0100

    rewrite mongodb query for grouped_nodes_timeseries
    this produces average of nodes across the network per day, and returns the date as a string

--- graphql_server/mongodb_queries/grouped_nodes_timeseries.js
+++ graphql_server/mongodb_queries/grouped_nodes_timeseries.js
@@ -8,8 +8,52 @@ const query = [
         }
     },
     {
+        $project: {
+            date: {
+                $dateToParts: {
+                    date: "$_id"
+                }
+            },
+            sumNodes: 1
+        }
+    },
+    {
+        $group: {
+            _id: {
+                date: {
+                    year: "$date.year",
+                    month: "$date.month",
+                    day: "$date.day"
+                }
+            },
+            avgNodes: {
+                $avg: "$sumNodes"
+            }
+        }
+    },
+    {
         $sort: {
             _id: 1
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            date: {
+                $dateToString: {
+                    format: "%Y-%m-%d",
+                    date: {
+                        $dateFromParts: {
+                            year: "$_id.date.year",
+                            month: "$_id.date.month",
+                            day: "$_id.date.day"
+                        }
+                    }
+                }
+            },
+            avgNodes: {
+                $toInt: "$avgNodes"
+            }
         }
     }
 ];
