6b84c33b4d0359bb230206d696b72c4225fd782a
Pierre Marshall <pierre.marshall@gmail.com>
2024-08-15 12:49:31 +0100

    add stackOrderDescending to graph3.js

--- visualisations/graphs/graph3.js
+++ visualisations/graphs/graph3.js
@@ -15,6 +15,7 @@ async function createGraph3(gql_query) {
     const parseUnixTime = d3.utcParse("%Y-%m-%d");
 
     const series = d3.stack()
+        .order(d3.stackOrderDescending)
         .keys(d3.union(gql_response_data.map(d => d.routingTech))) // distinct series keys, in input order
         .value(([, group], key) => group.get(key).seen) // get value for each series key and stack
         (d3.index(gql_response_data, d => parseUnixTime(d.date), d => d.routingTech)); // group by stack then series key
@@ -24,27 +25,27 @@ async function createGraph3(gql_query) {
         .domain(d3.extent(gql_response_data, d => parseUnixTime(d.date)))
         .range([marginLeft, width - marginRight]);
 
-        const y = d3.scaleLinear()
+    const y = d3.scaleLinear()
         .domain([0, d3.max(series, d => d3.max(d, d => d[1]))])
         .rangeRound([height - marginBottom, marginTop]);
-  
+
     const color = d3.scaleOrdinal()
         .domain(series.map(d => d.key))
         .range(d3.schemeTableau10);
-  
+
     // Construct an area shape.
     const area = d3.area()
         .x(d => x(d.data[0]))
         .y0(d => y(d[0]))
         .y1(d => y(d[1]));
-  
+
     // Create the SVG container.
     const svg = d3.create("svg")
         .attr("width", width)
         .attr("height", height)
         .attr("viewBox", [0, 0, width, height])
         .attr("style", "max-width: 100%; height: auto;");
-  
+
     // Add the y-axis, remove the domain line, add grid lines and a label.
     svg.append("g")
         .attr("transform", `translate(${marginLeft},0)`)
@@ -59,22 +60,22 @@ async function createGraph3(gql_query) {
             .attr("fill", "currentColor")
             .attr("text-anchor", "start")
             .text("Protocols in use"));
-  
+
     // Append a path for each series.
     svg.append("g")
-      .selectAll()
-      .data(series)
-      .join("path")
+        .selectAll()
+        .data(series)
+        .join("path")
         .attr("fill", d => color(d.key))
         .attr("d", area)
-      .append("title")
+        .append("title")
         .text(d => d.key);
-  
+
     // Append the horizontal axis atop the area.
     svg.append("g")
         .attr("transform", `translate(0,${height - marginBottom})`)
         .call(d3.axisBottom(x).tickSizeOuter(0));
-  
+
     // Return the chart with the color scale as a property (for the legend).
     // return Object.assign(svg.node(), {scales: {color}});
 
