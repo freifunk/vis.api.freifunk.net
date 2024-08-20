2d288f8354d09a6ebd84262941744978f9c8104a
Pierre Marshall <pierre.marshall@gmail.com>
2024-08-20 12:14:12 +0100

    rewrite latest_nodes_per_community query, fixes #58

--- graphql_server/mongodb_queries/latest_nodes_per_community.js
+++ graphql_server/mongodb_queries/latest_nodes_per_community.js
@@ -1,34 +1,24 @@
 const query = [
     // is there any appreciable performance gain
     // to projecting the fields before grouping?
+
     {
         $project: {
-            _id: 0,
+            _id: "$metadata",
             timestamp: true,
-            metadata: true,
             nodes: "$content.state.nodes"
         }
     },
     {
-        $group: {
-            _id: "$metadata",
-            timestamp: {
-                $first: "$timestamp"
-            },
-            nodes: {
-                $first: "$nodes"
-            }
-        }
-    },
-    {
         $sort: {
-            timestamp: 1,
+            timestamp: -1,
             nodes: -1
         }
     },
     {
         $limit: 10
     }
+
 ];
 
 module.exports = query;
